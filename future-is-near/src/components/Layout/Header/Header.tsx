a49d7d4743d45ce80ee6a08770cb78d19900f797
Andrew Chernigovsky <chernigovsky108@gmail.com>
2023-08-15 23:05:09 +0300

    navbar

--- future-is-near/src/components/Layout/Header/Header.tsx
+++ future-is-near/src/components/Layout/Header/Header.tsx
@@ -5,47 +5,57 @@ import styles from './Header.module.scss';
 
 const Header: FC = () => {
 	const [open, setOpened] = React.useState(true);
+	console.log(open);
 
 	return (
-		<header className={styles.header}>
-			<nav className={styles.nav}>
-				<button
-					type="button"
-					className={styles.nav__btn}
-					onClick={() => setOpened(!false)}
-				>
-					<span className="visually-hidden">Открыть меню</span>
-				</button>
-				<div className={open ? styles.nav__wrapper : ''}>
-					<ul className={styles.nav__list}>
-						<li>
-							<Link href="#">Features</Link>
-						</li>
-						<li>
-							<Link href="#">Customers</Link>
-						</li>
-						<li>
-							<Link href="/products">Pricing</Link>
-						</li>
-						<li>
-							<Link href="#">Company</Link>
-						</li>
-					</ul>
-					<ul className={styles.nav__auth}>
-						<li>
-							<Link color="inherit" href="#">
-								Login
-							</Link>
-						</li>
-						<li>
-							<Link color="inherit" href="#">
-								Sign up
-							</Link>
-						</li>
-					</ul>
-				</div>
-			</nav>
-		</header>
+		<>
+			<header className={styles.header}>
+				<nav className={styles.nav}>
+					<button
+						type="button"
+						className={styles.nav__btn}
+						onClick={() => setOpened(!open)}
+					>
+						<span></span>
+						<span className="visually-hidden">Открыть меню</span>
+					</button>
+					<div
+						className={
+							open
+								? styles.nav__wrapper + ' ' + styles.active
+								: styles.nav__wrapper
+						}
+					>
+						<ul className={styles.nav__list}>
+							<li>
+								<Link href="/features">Features</Link>
+							</li>
+							<li>
+								<Link href="/customers">Customers</Link>
+							</li>
+							<li>
+								<Link href="/products">Pricing</Link>
+							</li>
+							<li>
+								<Link href="/company">Company</Link>
+							</li>
+						</ul>
+					</div>
+						<ul className={styles.nav__auth}>
+							<li>
+								<Link color="inherit" href="/login">
+									Login
+								</Link>
+							</li>
+							<li>
+								<Link color="inherit" href="/signUp">
+									Sign up
+								</Link>
+							</li>
+						</ul>
+				</nav>
+			</header>
+		</>
 	);
 };
 
