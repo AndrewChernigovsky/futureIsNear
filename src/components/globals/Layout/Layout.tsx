2e2fa50fac3cf9798fdfba18d3f562ca5423469d
Andrew Chernigovsky <chernigovsky108@gmail.com>
2023-10-11 20:15:31 +0300

    not finished yet

--- src/components/globals/Layout/Layout.tsx
+++ src/components/globals/Layout/Layout.tsx
@@ -14,7 +14,7 @@ const Layout: FC<ILayout> = ({ children, showSidebar }) => {
 		<>
 			<div className={styles.wrapper}>
 				<Header />
-				{showSidebar ? <Sidebar /> : ''}
+				{showSidebar ? <Sidebar active /> : ''}
 				<main className={styles.container}>{children}</main>
 				<Footer />
 			</div>
--- src/components/globals/Layout/Sidebar/Sidebar.tsx
+++ src/components/globals/Layout/Sidebar/Sidebar.tsx
@@ -3,6 +3,7 @@ import styles from './Sidebar.module.scss'
 import React from 'react'
 import Link from 'next/link'
 import { categories } from './data'
+import { useRouter } from 'next/navigation'
 
 interface ICategory {
 	href: string
@@ -13,15 +14,24 @@ interface ICategory {
 	desc: string
 }
 
-const Sidebar: FC = () => {
+interface Props {
+	active: boolean
+}
+const Sidebar: FC<Props> = (active: Props) => {
+	const router = useRouter()
+	let conditionalColor = active ? 'red' : 'blue'
 	return (
 		<aside className={styles.sidebar}>
+			<p>{router.path}</p>
 			<ul className={styles.sidebar__wrapper}>
 				{categories.map((el: ICategory, index) => {
 					return (
 						<li key={index}>
-							<Link color="inherit" href={el.href}>
-								{el.image((el.size = 25), (el.color = 'white'))}
+							<Link href={el.href}>
+								{el.image(
+									(el.size = 25),
+									(el.color = conditionalColor),
+								)}
 								<span className="visually-hidden">
 									{el.desc}
 								</span>
