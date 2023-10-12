857e7563543dc5b6efa0612cd40338d233f7f63a
Ramil' Osmanov <osmanovramil823@gmail.com>
2023-10-13 00:42:20 +0600

    api routes

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -1,36 +1,36 @@
-import { prisma } from '../../../../prisma/prisma'
+// import { prisma } from '../../../../prisma/prisma'
 
-export async function POST(req: Request, res: Response) {
-	const body = await req.json()
-	const { email, password, name } = body
+// export async function POST(req: Request, res: Response) {
+// 	const body = await req.json()
+// 	const { email, password, name } = body
 
-	const user: TUser = await prisma.user.findUnique({
-		where: {
-			email,
-			password,
-		},
-	})
+// 	const user: TUser = await prisma.user.findUnique({
+// 		where: {
+// 			email,
+// 			password,
+// 		},
+// 	})
 
-	if (user) {
-		return new Response(
-			JSON.stringify({
-				msg: 'Пользователь уже зарегистрирован',
-				data: user,
-			}),
-		)
-	} else {
-		const user: TUser = await prisma.user.create({
-			data: {
-				name,
-				password,
-				email,
-				isVerified: false,
-				role: 'user',
-			},
-		})
+// 	if (user) {
+// 		return new Response(
+// 			JSON.stringify({
+// 				msg: 'Пользователь уже зарегистрирован',
+// 				data: user,
+// 			}),
+// 		)
+// 	} else {
+// 		const user: TUser = await prisma.user.create({
+// 			data: {
+// 				name,
+// 				password,
+// 				email,
+// 				isVerified: false,
+// 				role: 'user',
+// 			},
+// 		})
 
-		return new Response(
-			JSON.stringify({ msg: 'Пользователь создан успешно!', data: user }),
-		)
-	}
-}
+// 		return new Response(
+// 			JSON.stringify({ msg: 'Пользователь создан успешно!', data: user }),
+// 		)
+// 	}
+// }
